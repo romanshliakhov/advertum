b5358cbde17ebc952b3bfd5db1d030097b038e98
romanshliakhov <romanshliakhov@gmail.com>
2022-02-23 16:04:31 +0200

    update slider1

--- source/js/common.js
+++ source/js/common.js
@@ -2,6 +2,7 @@
 let banner = new Swiper('.missions__slider', {
   slidesPerView: 5,
   loop: true,
+  spaceBetween: 15,
   navigation: {
     prevEl: '.missions__slider-btn--prev',
     nextEl: '.missions__slider-btn--next',
